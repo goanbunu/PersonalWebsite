3ea1a51b578834be6f696f0330a5acfbb148582b
Larissa Muramoto <larissa.muramoto@gmail.com>
2015-12-09 17:39:18 -0800

    use compressed images

--- js/configuration.js
+++ js/configuration.js
@@ -12,24 +12,24 @@ $(function(){
                 'description'   : '<ul><li>Simply Hired user accounts allow people to upload/edit their resume, save/apply to jobs, manage saved searches, and sign up for email alerts.</li> \
                 <li>Content is responsive and dynamically rendered to ensure a positive UX.</li><li>Built with Backbone.js, Handlebars, Sass, and Django REST Framework.</li></ul>',
                 'thumbnail'     : [
-                    'images/thumbs/accounts_savedjobs.png',
-                    'images/thumbs/accounts_noresume.png',
-                    'images/thumbs/accounts_resumeupload.png',
-                    'images/thumbs/accounts_resume.png',
-                    'images/thumbs/accounts_editresume.png',
-                    'images/thumbs/accounts_resumemobile.png',
-                    'images/thumbs/accounts_savedjobsmobile.png',
-                    'images/thumbs/accounts_mobilemenu.png'
+                    'images/thumbs/accounts_savedjobs.gif',
+                    'images/thumbs/accounts_noresume.gif',
+                    'images/thumbs/accounts_resumeupload.gif',
+                    'images/thumbs/accounts_resume.gif',
+                    'images/thumbs/accounts_editresume.gif',
+                    'images/thumbs/accounts_resumemobile.gif',
+                    'images/thumbs/accounts_savedjobsmobile.gif',
+                    'images/thumbs/accounts_mobilemenu.gif'
                 ],
                 'large'         : [
-                    'images/screenshots/accounts_savedjobs.png',
-                    'images/screenshots/accounts_noresume.png',
-                    'images/screenshots/accounts_resumeupload.png',
-                    'images/screenshots/accounts_resume.png',
-                    'images/screenshots/accounts_editresume.png',
-                    'images/screenshots/accounts_resumemobile.png',
-                    'images/screenshots/accounts_savedjobsmobile.png',
-                    'images/screenshots/accounts_mobilemenu.png'
+                    'images/screenshots/accounts_savedjobs.gif',
+                    'images/screenshots/accounts_noresume.gif',
+                    'images/screenshots/accounts_resumeupload.gif',
+                    'images/screenshots/accounts_resume.gif',
+                    'images/screenshots/accounts_editresume.gif',
+                    'images/screenshots/accounts_resumemobile.gif',
+                    'images/screenshots/accounts_savedjobsmobile.gif',
+                    'images/screenshots/accounts_mobilemenu.gif'
                 ],
                 'button_list'   : [],
                 'tags'          : []
@@ -41,20 +41,20 @@ $(function(){
                     <li>The decoration part of the app is built with JavaScript and HTML5 Canvas. The app detects the cursor position to position selected graphics on the image. When a user is done, they can download, save, or email the image.</li> \
                 <li>Built with OpenCV, JavaScript, HTML5 Canvas, Python, and Flask</li></ul>',
                 'thumbnail'     : [
-                    'images/thumbs/opencv_thumb.png',
+                    'images/thumbs/opencv_thumb.jpg',
                     'images/thumbs/opencv_animated.gif',
-                    'images/thumbs/opencv_decoratinginterface.png',
-                    'images/thumbs/opencv_pens.png',
-                    'images/thumbs/opencv_stamps.png',
-                    'images/thumbs/opencv_homepage.png',
+                    'images/thumbs/opencv_decoratinginterface.jpg',
+                    'images/thumbs/opencv_pens.jpg',
+                    'images/thumbs/opencv_stamps.jpg',
+                    'images/thumbs/opencv_homepage.jpg',
                 ],
                 'large'         : [
-                    'images/screenshots/opencv_process.png',
+                    'images/screenshots/opencv_process.jpg',
                     'images/screenshots/opencv_animated.gif',
-                    'images/screenshots/opencv_decoratinginterface.png',
-                    'images/screenshots/opencv_pens.png',
-                    'images/screenshots/opencv_stamps.png',
-                    'images/screenshots/opencv_homepage.png',
+                    'images/screenshots/opencv_decoratinginterface.jpg',
+                    'images/screenshots/opencv_pens.jpg',
+                    'images/screenshots/opencv_stamps.jpg',
+                    'images/screenshots/opencv_homepage.jpg',
                 ],
                 'button_list'   :
                 [
@@ -69,12 +69,12 @@ $(function(){
                   <li>Users can either click on the map directly or click an eatery name from the menu on the right.</li> \
                   <li>Built with JavaScript, HTML, CSS, and Photoshop</li></ul>',
                 'thumbnail'     : [
-                    'images/thumbs/diningmap_unselected.png',
-                    'images/thumbs/diningmap_selected.png',
+                    'images/thumbs/diningmap_unselected.jpg',
+                    'images/thumbs/diningmap_selected.jpg',
                 ],
                 'large'         : [
-                    'images/screenshots/diningmap_unselected.png',
-                    'images/screenshots/diningmap_selected.png',
+                    'images/screenshots/diningmap_unselected.jpg',
+                    'images/screenshots/diningmap_selected.jpg',
                 ],
                 'button_list'   : [],
                 'tags'          : []
@@ -86,20 +86,14 @@ $(function(){
                   <li>Users can use Karma to automate running a suite of front end unit tests powered by Jasmine</li> \
                   <li>Built with Backbone.js, Handlebars, Sass, Jasmine, Karma, Django, and Django REST Framework.</li></ul>',
                 'thumbnail'     : [
-                    'images/thumbs/carousel_image1.png',
-                    'images/thumbs/carousel_image2.png',
-                    'images/thumbs/carousel_image3.png',
-                    'images/thumbs/carousel_image4.png',
-                    'images/thumbs/carousel_image5.png',
-                    'images/thumbs/carousel_image6.png'
+                    'images/thumbs/carousel_image1.jpg',
+                    'images/thumbs/carousel_image2.jpg',
+                    'images/thumbs/carousel_image3.jpg',
                 ],
                 'large'         : [
-                    'images/screenshots/carousel_image1.png',
-                    'images/screenshots/carousel_image2.png',
-                    'images/screenshots/carousel_image3.png',
-                    'images/screenshots/carousel_image4.png',
-                    'images/screenshots/carousel_image5.png',
-                    'images/screenshots/carousel_image6.png'
+                    'images/screenshots/carousel_image1.jpg',
+                    'images/screenshots/carousel_image2.jpg',
+                    'images/screenshots/carousel_image3.jpg',
                 ],
                 'button_list'   :
                 [
